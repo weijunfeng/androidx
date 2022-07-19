27f01311f7417d460ab8ff950b63261850d53aed
Aurimas <aurimas@liutikas.net>
2022-07-19 16:53:56 -0700

    Remove unneeded Gradle transform

--- buildSrc/private/src/main/kotlin/androidx/build/AndroidXPlaygroundRootImplPlugin.kt
+++ buildSrc/private/src/main/kotlin/androidx/build/AndroidXPlaygroundRootImplPlugin.kt
@@ -56,7 +56,6 @@ class AndroidXPlaygroundRootImplPlugin : Plugin<Project> {
         config = PlaygroundProperties.load(rootProject)
         repos = PlaygroundRepositories(config)
         rootProject.repositories.addPlaygroundRepositories()
-        GradleTransformWorkaround.maybeApply(rootProject)
         rootProject.subprojects {
             configureSubProject(it)
         }
